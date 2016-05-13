92c8042ba2f08fbb7e358cf129f5b5f12bcbc8ef
unknowntg <arashnomiri@gmail.com>
2016-05-13 20:13:27 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'teleseed' then -- Put everything you like :)
+  if matches[1]:lower() == 'public' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /public ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -120,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "teleseed" then -- Put everything you like :)
+    if matches[2] == "public" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
